78adb4289aa33467ce0794cf1010b56a24f76be6
mavi <mavi.baris96@gmail.com>
2021-11-24 06:47:01 +0300

    Add authRoute

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const authRoute = require("./routes/authRoute");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use("/auth", authRoute);
 
 app.listen(5000, () =>
   console.log("App is listening on url http://localhost:5000")
