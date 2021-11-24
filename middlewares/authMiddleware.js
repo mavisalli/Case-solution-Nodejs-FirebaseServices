de3d90c0eab162ca4541d575b87087a2311b81f4
mavi <mavi.baris96@gmail.com>
2021-11-24 06:43:13 +0300

    Create authMiddleware for security

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,33 @@
+const admin = require("firebase-admin");
+const db = require("../db");
+
+const getAuthToken = (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    req.authToken = req.headers.authorization.split(" ")[1];
+  } else {
+    req.authToken = null;
+  }
+  next();
+};
+
+const checkIfAuthenticated = (req, res, next) => {
+  getAuthToken(req, res, async () => {
+    try {
+      const { authToken } = req;
+      const userInfo = await admin.auth().verifyIdToken(authToken);
+      req.user = userInfo;
+      return next();
+    } catch (e) {
+      return res
+        .status(401)
+        .send({ error: "You are not authorized to make this request" });
+    }
+  });
+};
+
+module.exports = {
+  checkIfAuthenticated,
+};
