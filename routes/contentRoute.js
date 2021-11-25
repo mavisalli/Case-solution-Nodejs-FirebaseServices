0407b86f1da6f13f030f9bdce37067732b211a46
mavi <mavi.baris96@gmail.com>
2021-11-25 03:44:19 +0300

    Add validation schemes created specifically for the endpoints themselves

--- routes/contentRoute.js
+++ routes/contentRoute.js
@@ -9,23 +9,36 @@ const {
   addEpisode,
 } = require("../controllers/contentController");
 
+const schemas = require("../handlers/validation");
+const validate = require("../middlewares/validate");
+
 const { checkIfAuthenticated } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/content").post(checkIfAuthenticated, createContent);
+router
+  .route("/content")
+  .post(validate(schemas.contentSchema), checkIfAuthenticated, createContent);
 
 router.route("/contents").get(checkIfAuthenticated, getContents);
 
-router.route("/content/:id/season").put(checkIfAuthenticated, addSeason);
+router
+  .route("/content/:id/season")
+  .put(validate(schemas.addSeasonsSchema), checkIfAuthenticated, addSeason);
 
 router
   .route("/content/:id/season/:seasonIndex/episode")
-  .put(checkIfAuthenticated, addEpisode);
+  .put(validate(schemas.addEpisodesSchema), checkIfAuthenticated, addEpisode);
 
 router.route("/content/:id").get(checkIfAuthenticated, getContentDetail);
 
-router.route("/content/:id").patch(checkIfAuthenticated, updateContent);
+router
+  .route("/content/:id")
+  .patch(
+    validate(schemas.updateContentSchema),
+    checkIfAuthenticated,
+    updateContent
+  );
 
 router.route("/content/:id").delete(checkIfAuthenticated, deleteContent);
 
