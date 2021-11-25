9bf4c763a69bf46322d0ac515277a413491b7063
mavi <mavi.baris96@gmail.com>
2021-11-25 20:54:52 +0300

    Use bcrypt to hide admin password saved in firestore

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,41 @@
 const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 const admin = require("firebase-admin");
 const db = require("../db");
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 exports.signupAdmin = async (req, res) => {
   try {
-    const { email, password, firstName, lastName } = req.body;
+    const {
+      email,
+      password,
+      phoneNumber,
+      photoUrl,
+      firstName,
+      lastName,
+      birthDay,
+    } = req.body;
 
     const user = await admin.auth().createUser({
       email,
       password,
+      photoUrl,
+      phoneNumber,
       displayName: `${firstName} ${lastName}`,
     });
+
+    if (user) {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        await db.collection("admins").doc(user.uid).create({
+          email,
+          firstName,
+          lastName,
+          phoneNumber,
+          password: hash,
+          birthDay,
+        });
+      });
+    }
 
     res.send(user);
   } catch (error) {
