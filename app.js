db6dceaa0591ebb14c8b2116656d5ef908705bd0
mavi <mavi.baris96@gmail.com>
2021-11-26 18:08:37 +0300

    Add descriptive comment lines

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerJsDoc = YAML.load("./api.yaml");
 
+//Routes Import
 const authRoute = require("./routes/authRoute");
 const contentRoute = require("./routes/contentRoute");
 
+// Initialize app
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(logger("dev"));
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 
+// Router paths
 app.use("/", contentRoute);
 app.use("/auth", authRoute);
 
